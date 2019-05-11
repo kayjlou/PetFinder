ed82280f5605bd408a589b8e633fe04aa7f0e6ef
Katie Lewis <klewi314@gmail.com>
2019-05-11 09:44:24 -0700

    Added server requires

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser")
+const { join } = require("path");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.static(join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//Points our server to a series of route files
+//These routes give our server a map of how to respond when users visit or request data from various urls
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
+routes(app);
+
+app.listen(process.env.PORT || 3000);
