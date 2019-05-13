5aa7989d9619b94e0fb8061863d9e3678459a88c
Katie Lewis <klewi314@gmail.com>
2019-05-12 19:14:47 -0700

    Removed questions for testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,10 +13,11 @@ module.exports = function(app) {
 
   //Handles post requests
   app.post("/api/pets", (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
 
     //Recieve user values (name, photo and scores)
     let newUser = req.body;
+    let userScores = newUser.scores;
 
     // Parse int scores
     // for (let i = 0; i < newUser.scores.length; i++) {
@@ -24,28 +25,35 @@ module.exports = function(app) {
     // }
 
     //Set match initial value
-    let match = {
+    match = {
       name: "",
       photo: "",
       matchDifference: 1000
     };
 
-    let newScores = newUser.scores;
-
+    let difference;
     //Loops through current pets to get scores
     for (i = 0; i < pets.length; i++) {
-      let difference = 1000;
-
+      let currentPet = pets[i];
+      difference = 0;
+      console.log(currentPet.name);
       //check compatability by subtracting each question and then adding together total difference
       for (let j = 0; j < newUser.scores.length; j++) {
+        let currentPetScore = currentPet.Score;
+        let currentUserScore = userScores[j];
         difference += Math.abs(
-          parseInt(newScores[i]) - parseInt(pets[i].scores)
+          parseInt(currentUserScore) - parseInt(currentPetScore)
         );
-        console.log(difference);
+        console.log(`Calculating difference`);
+        console.log(`The difference is ${difference}`);
+        //Set new match info
+        if (difference < match.matchDifference) {
+          match.name = currentPet.name;
+          match.photo = currentPet.photo;
+          console.log(`The new match is ${match.name}`);
+        }
       }
     }
-    //Set new match info
-
     //Post new data that user inputs
     pets.push(newUser);
   });
