fa70160807aa98f6694bcc213682d71d3edef63e
Katie Lewis <klewi314@gmail.com>
2019-05-12 18:46:39 -0700

    need to fix match equation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,7 +12,17 @@ module.exports = function(app) {
   });
 
   //Handles post requests
-  app.post("/api/friends", (req, res) => {
+  app.post("/api/pets", (req, res) => {
+    console.log(req.body);
+
+    //Recieve user values (name, photo and scores)
+    let newUser = req.body;
+
+    // Parse int scores
+    // for (let i = 0; i < newUser.scores.length; i++) {
+    //   newUser.scores[i] = parseInt(newUser.scores[i]);
+    // }
+
     //Set match initial value
     let match = {
       name: "",
@@ -20,10 +30,23 @@ module.exports = function(app) {
       matchDifference: 1000
     };
 
-    //Post new data that user inputs
+    let newScores = newUser.scores;
 
-    //check compatability
+    //Loops through current pets to get scores
+    for (i = 0; i < pets.length; i++) {
+      let difference = 1000;
 
+      //check compatability by subtracting each question and then adding together total difference
+      for (let j = 0; j < newUser.scores.length; j++) {
+        difference += Math.abs(
+          parseInt(newScores[i]) - parseInt(pets[i].scores)
+        );
+        console.log(difference);
+      }
+    }
     //Set new match info
+
+    //Post new data that user inputs
+    pets.push(newUser);
   });
 };
