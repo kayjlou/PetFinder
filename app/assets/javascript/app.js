8289241825828964154eb50bdc5fcb4eebdf5a1f
Katie Lewis <klewi314@gmail.com>
2019-05-13 19:06:39 -0700

    added seperate javascript file

---
+++ app/assets/javascript/app.js
@@ -0,0 +1,50 @@
+const showMatch = match => {
+  //Hide survey
+  document.querySelector(".survey").style.display = "none";
+  document.querySelector("#info").style.display = "none";
+  document.querySelector(".input-div").style.display = "none";
+  document.querySelector("#matchName").innerHTML = `Your match is ${
+    match.name
+  }!`;
+};
+
+const getDiff = input => {
+  // let newUser = req.body;
+  let newScores = input.scores;
+  //Set match initial value
+  match = {
+    name: "",
+    photo: "",
+    matchDifference: 1000
+  };
+
+  let difference = "";
+
+  //Loops through current pets to get scores
+  for (i = 0; i < pets.length; i++) {
+    let currentPet = pets[i];
+    difference = 0;
+    console.log(`Running compatibility with ${currentPet.name}`);
+
+    //check compatability by subtracting each question and then adding together total difference
+    for (let j = 0; j < newScores.length; j++) {
+      let currentPetScore = currentPet.scores[j];
+      let currentUserScore = newScores[j];
+      difference += Math.abs(
+        parseInt(currentUserScore) - parseInt(currentPetScore)
+      );
+      console.log(
+        `Calculating difference ${currentUserScore} minus ${currentPetScore}`
+      );
+      console.log(`The difference is ${difference}`);
+
+      //Set new match info
+      if (difference < match.matchDifference) {
+        match.name = currentPet.name;
+        match.photo = currentPet.photo;
+        console.log(`The new match is ${match.name}`);
+      }
+    }
+  }
+  showMatch(match);
+};
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -65,54 +65,54 @@ module.exports = function(app) {
     console.log(`New user pushed: ${newUser.name}`);
   });
 
-  const getDiff = input => {
-    // let newUser = req.body;
-    let newScores = input.scores;
-    //Set match initial value
-    match = {
-      name: "",
-      photo: "",
-      matchDifference: 1000
-    };
+  // const getDiff = input => {
+  //   // let newUser = req.body;
+  //   let newScores = input.scores;
+  //   //Set match initial value
+  //   match = {
+  //     name: "",
+  //     photo: "",
+  //     matchDifference: 1000
+  //   };
 
-    let difference = "";
+  //   let difference = "";
 
-    //Loops through current pets to get scores
-    for (i = 0; i < pets.length; i++) {
-      let currentPet = pets[i];
-      difference = 0;
-      console.log(`Running compatibility with ${currentPet.name}`);
+  //   //Loops through current pets to get scores
+  //   for (i = 0; i < pets.length; i++) {
+  //     let currentPet = pets[i];
+  //     difference = 0;
+  //     console.log(`Running compatibility with ${currentPet.name}`);
 
-      //check compatability by subtracting each question and then adding together total difference
-      for (let j = 0; j < newScores.length; j++) {
-        let currentPetScore = currentPet.scores[j];
-        let currentUserScore = newScores[j];
-        difference += Math.abs(
-          parseInt(currentUserScore) - parseInt(currentPetScore)
-        );
-        console.log(
-          `Calculating difference ${currentUserScore} minus ${currentPetScore}`
-        );
-        console.log(`The difference is ${difference}`);
+  //     //check compatability by subtracting each question and then adding together total difference
+  //     for (let j = 0; j < newScores.length; j++) {
+  //       let currentPetScore = currentPet.scores[j];
+  //       let currentUserScore = newScores[j];
+  //       difference += Math.abs(
+  //         parseInt(currentUserScore) - parseInt(currentPetScore)
+  //       );
+  //       console.log(
+  //         `Calculating difference ${currentUserScore} minus ${currentPetScore}`
+  //       );
+  //       console.log(`The difference is ${difference}`);
 
-        //Set new match info
-        if (difference < match.matchDifference) {
-          match.name = currentPet.name;
-          match.photo = currentPet.photo;
-          console.log(`The new match is ${match.name}`);
-        }
-      }
-    }
-    showMatch(match);
-  };
+  //       //Set new match info
+  //       if (difference < match.matchDifference) {
+  //         match.name = currentPet.name;
+  //         match.photo = currentPet.photo;
+  //         console.log(`The new match is ${match.name}`);
+  //       }
+  //     }
+  //   }
+  //   showMatch(match);
+  // };
 
-  const showMatch = match => {
-    //Hide survey
-    document.querySelector(".survey").style.display = "none";
-    document.querySelector("#info").style.display = "none";
-    document.querySelector(".input-div").style.display = "none";
-    document.querySelector("#matchName").innerHTML = `Your match is ${
-      match.name
-    }!`;
-  };
+  // const showMatch = match => {
+  //   //Hide survey
+  //   document.querySelector(".survey").style.display = "none";
+  //   document.querySelector("#info").style.display = "none";
+  //   document.querySelector(".input-div").style.display = "none";
+  //   document.querySelector("#matchName").innerHTML = `Your match is ${
+  //     match.name
+  //   }!`;
+  // };
 };
