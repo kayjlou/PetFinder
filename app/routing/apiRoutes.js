f483244cfc64af118b52dc4a359c9c4643190cc3
Katie Lewis <klewi314@gmail.com>
2019-05-13 19:04:22 -0700

    Added function to calculate difference

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,5 @@
 //Link routes to data
 
-
 const pets = require("../data/pets");
 
 module.exports = function(app) {
@@ -18,13 +17,57 @@ module.exports = function(app) {
 
     //Recieve user values (name, photo and scores)
     let newUser = req.body;
-    let newScores = newUser.scores;
+    // let newScores = newUser.scores;
 
     // Parse int scores
     // for (let i = 0; i < newUser.scores.length; i++) {
     //   newUser.scores[i] = parseInt(newUser.scores[i]);
     // }
 
+    //Set match initial value
+    // match = {
+    //   name: "",
+    //   photo: "",
+    //   matchDifference: 1000
+    // };
+
+    // let difference = "";
+
+    // //Loops through current pets to get scores
+    // for (i = 0; i < pets.length; i++) {
+    //   let currentPet = pets[i];
+    //   difference = 0;
+    //   console.log(`Running compatibility with ${currentPet.name}`);
+
+    //   //check compatability by subtracting each question and then adding together total difference
+    //   for (let j = 0; j < newScores.length; j++) {
+    //     let currentPetScore = currentPet.scores[j];
+    //     let currentUserScore = newScores[j];
+    //     difference += Math.abs(
+    //       parseInt(currentUserScore) - parseInt(currentPetScore)
+    //     );
+    //     console.log(
+    //       `Calculating difference ${currentUserScore} minus ${currentPetScore}`
+    //     );
+    //     console.log(`The difference is ${difference}`);
+
+    //     //Set new match info
+    //     if (difference < match.matchDifference) {
+    //       match.name = currentPet.name;
+    //       match.photo = currentPet.photo;
+    //       console.log(`The new match is ${match.name}`);
+    //     }
+    //   }
+    // }
+    getDiff(newUser);
+    //Post new data that user inputs
+    pets.push(newUser);
+    console.log(`New user pushed: ${newUser.name}`);
+  });
+
+  const getDiff = input => {
+    // let newUser = req.body;
+    let newScores = input.scores;
     //Set match initial value
     match = {
       name: "",
@@ -60,14 +103,16 @@ module.exports = function(app) {
         }
       }
     }
-    //Post new data that user inputs
-    pets.push(newUser);
-    console.log(`New user pushed: ${newUser.name}`);
+    showMatch(match);
+  };
 
-    //J Query info from modal and post info
-    //J Query info from modal and post info
-    $("#matchName").text(data.name);
-    $("#matchImg").attr("src", data.photo);
-    $("#matchModal").modal("toggle");
-  });
+  const showMatch = match => {
+    //Hide survey
+    document.querySelector(".survey").style.display = "none";
+    document.querySelector("#info").style.display = "none";
+    document.querySelector(".input-div").style.display = "none";
+    document.querySelector("#matchName").innerHTML = `Your match is ${
+      match.name
+    }!`;
+  };
 };
