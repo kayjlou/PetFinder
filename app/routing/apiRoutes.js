1da4e716a648727e8e44df803cecc67a22ce9ca8
Katie Lewis <klewi314@gmail.com>
2019-05-15 17:54:05 -0700

    Cleaned up routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
 //Link routes to data
 
 const pets = require("../data/pets");
-const { getDiff, showMatch } = require("../assets/javascript/app.js");
+// const { getDiff, showMatch } = require("../assets/javascript/app.js");
 
 module.exports = function(app) {
   // API GET requests
   // handles when user visits a page
-  // In each of the below cases whena user visits a link
+  // In each of the below cases when a user visits a link
   app.get("/api/pets", (req, res) => {
     console.log("Getting pets");
     res.json(pets);
@@ -14,17 +14,17 @@ module.exports = function(app) {
 
   //Handles post requests
   app.post("/api/pets", (req, res) => {
-    // console.log(req.body);
-
     //Recieve user values (name, photo and scores)
     let newUser = req.body;
-    // let newScores = newUser.scores;
     //Run function to find out who is the best match
-    getDiff(newUser);
+    // getDiff(newUser);
 
     //Post new data that user inputs
     pets.push(newUser);
     console.log(`New user pushed: ${newUser.name}`);
-    showMatch(newUser);
+    res.sendStatus(200);
+    // .then(_ => res.sendStatus(200))
+    // .catch(e => console.log(e));
+    // showMatch(newUser);
   });
 };
