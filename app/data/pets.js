06e2928af76cb67db4cad02279bceb231a54b2f0
Katie Lewis <klewi314@gmail.com>
2019-07-23 12:26:50 -0700

    Updated scores in pets

--- app/data/pets.js
+++ app/data/pets.js
@@ -3,37 +3,37 @@ const pets = [
     name: "Ahmed",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1]
+    scores: [5, 1, 5]
   },
   {
     name: "Bomb",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [2, 5]
+    scores: [2, 5, 4]
   },
   {
     name: "Bomb",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [2, 5]
+    scores: [2, 5, 6]
   },
   {
     name: "Bomb",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [2, 5]
+      "https://secure.img1-fg.wfcdn.com/im/54313211/resize-h600%5Ecompr-r85/4307/43074644/Sleeping++French+Bulldog+Puppy+Statue.jpg",
+    scores: [2, 5, 6]
   },
   {
     name: "Bomb",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [2, 5]
+      "https://secure.img1-fg.wfcdn.com/im/54313211/resize-h600%5Ecompr-r85/4307/43074644/Sleeping++French+Bulldog+Puppy+Statue.jpg",
+    scores: [2, 5, 5]
   },
   {
     name: "Bomb",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [2, 5]
+      "https://secure.img1-fg.wfcdn.com/im/54313211/resize-h600%5Ecompr-r85/4307/43074644/Sleeping++French+Bulldog+Puppy+Statue.jpg",
+    scores: [2, 5, 5]
   }
 ];
 
