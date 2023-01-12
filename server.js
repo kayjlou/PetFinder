89bcb5edd727a7c46a2fee096c16a29c9c9a165c
Katie Lewis <117208660+sonakatie@users.noreply.github.com>
2023-01-11 16:27:12 -0800

    clean up

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(join(__dirname, "/app")));
+app.use(express.static(join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Points our server to a series of route files
 //These routes give our server a map of how to respond when users visit or request data from various urls
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 // routes(app);
 //Listen on port and log out link to click
